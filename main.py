46e762890d893acf8f410243ea216ff6f85e6201
xiangzhendong <zhendong.xiang@gmail.com>
2015-12-26 11:07:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 # -*- coding:utf-8 -*-
 #encoding = utf-8
 from bottle import Bottle, get, post, request
-
 import MySQLdb
-
 import sae.const
-
 import hashlib
-
 import xml.etree.ElementTree as ET
 
 MYSQL_DB=sae.const.MYSQL_DB
@@ -18,6 +14,7 @@ MYSQL_HOST_S=sae.const.MYSQL_HOST_S
 MYSQL_PORT=int(sae.const.MYSQL_PORT)
 
 app=Bottle()
+
 
 @app.route('/', method=['GET'])
 def check_signature():
@@ -69,7 +66,7 @@ def index():
             <Content><![CDATA[{}]]></Content></xml>
             '''.format(recv_con['FromUserName'],recv_con['ToUserName'], ret_msg)
             return myxml
-
+        
 
         if row is None:
             try:
@@ -78,11 +75,11 @@ def index():
 
                 connection.close()
 
-                ret_msg = 'insert_success! '
+                ret_msg = '您的用户名已经添加成功啦^_^ '
             except Exception, e:
                 ret_msg = 'error: insert_error:' + e.__class__.__doc__
         else:
-            ret_msg = '您已添加过用户名!'
+            ret_msg = '已经有你的名字喽^_^'
 
         myxml = '''\
         <xml>
@@ -93,7 +90,58 @@ def index():
         <Content><![CDATA[{}]]></Content></xml>
         '''.format(recv_con['FromUserName'],recv_con['ToUserName'], ret_msg)
         return myxml
+
+    elif d.startswith('b '):
+        row = None
+
+        try:
+            userid = recv_con['FromUserName']
+            connection=MySQLdb.connect(host=MYSQL_HOST_M, port=MYSQL_PORT, \
+                                       user=MYSQL_USER, passwd=MYSQL_PASS, \
+                                       charset="utf8")
+            connection.select_db(MYSQL_DB)
+
+            cur = connection.cursor()
+            sql_select="""SELECT * FROM user_table WHERE openid='"""+userid+"""'"""
+            cur.execute(sql_select)
+            row = cur.fetchone()
+
+        except Exception, e:
+            ret_msg = 'error: open_mysql_error' + e.__class__.__doc__
+            myxml = '''\
+             <xml>
+             <ToUserName><![CDATA[{}]]></ToUserName>
+             <FromUserName><![CDATA[{}]]></FromUserName>
+             <CreateTime>12345678</CreateTime>
+             <MsgType><![CDATA[text]]></MsgType>
+             <Content><![CDATA[{}]]></Content></xml>
+             '''.format(recv_con['FromUserName'],recv_con['ToUserName'], ret_msg)
+            return myxml
         
+
+        if row is not None:
+            try:
+                sql_insert="""insert into book_table(datetime, bookname, username, openid) VALUES (CURRENT_TIMESTAMP,'"""+d[2:]+"""','"""+row[2]+"""','"""+userid+"""')"""
+                cur.execute(sql_insert)
+
+                connection.close()
+ 
+                ret_msg = '您的图书已经添加成功啦^_^'
+            except Exception, e:
+                ret_msg = 'error: insert_error:' + e.__class__.__doc__
+        else:
+            ret_msg = '要先添加用户名哦^_^'
+ 
+        myxml = '''\
+            <xml>
+            <ToUserName><![CDATA[{}]]></ToUserName>
+            <FromUserName><![CDATA[{}]]></FromUserName>
+            <CreateTime>12345678</CreateTime>
+            <MsgType><![CDATA[text]]></MsgType>
+            <Content><![CDATA[{}]]></Content></xml>
+            '''.format(recv_con['FromUserName'],recv_con['ToUserName'], ret_msg)
+        return myxml
+    
     elif d in ['qu']:
         try:
             connection=MySQLdb.connect(host=MYSQL_HOST_M, port=MYSQL_PORT, \
@@ -135,13 +183,144 @@ def index():
             <Content><![CDATA[{}]]></Content></xml>
             '''.format(recv_con['FromUserName'],recv_con['ToUserName'], ret_msg)
             return myxml
-                                      
+
+    elif d.startswith('qu '):
+        try:
+            connection=MySQLdb.connect(host=MYSQL_HOST_M, port=MYSQL_PORT, \
+                                       user=MYSQL_USER, passwd=MYSQL_PASS, \
+                                       charset="utf8")
+            connection.select_db(MYSQL_DB)
+
+            cur = connection.cursor()
+
+            sql_select = """SELECT * FROM book_table"""
+            cur.execute(sql_select)
+
+            ret_msg = ''
+            for record in cur.fetchall():
+                if d[3:]==record[3]:
+                    u=record[2].encode("utf-8")
+                    ret_msg = ret_msg + u + '\n'
+        except Exception, e:
+            ret_msg = 'error: insert_error:' + e.__class__.__doc__
+
+        try:
+            myxml = '''\
+            <xml>
+            <ToUserName><![CDATA[{}]]></ToUserName>
+            <FromUserName><![CDATA[{}]]></FromUserName>
+            <CreateTime>12345678</CreateTime>
+            <MsgType><![CDATA[text]]></MsgType>
+            <Content><![CDATA[{}]]></Content></xml>
+            '''.format(recv_con['FromUserName'],recv_con['ToUserName'], ret_msg)
+            return myxml
+        except Exception, e:
+            ret_msg = ret_msg + e.__class__.__doc__
+
+            myxml = '''\
+            <xml>
+            <ToUserName><![CDATA[{}]]></ToUserName>
+            <FromUserName><![CDATA[{}]]></FromUserName>
+            <CreateTime>12345678</CreateTime>
+            <MsgType><![CDATA[text]]></MsgType>
+            <Content><![CDATA[{}]]></Content></xml>
+            '''.format(recv_con['FromUserName'],recv_con['ToUserName'], ret_msg)
+            return myxml
+
+    elif d in ['qb']:
+        try:
+            connection=MySQLdb.connect(host=MYSQL_HOST_M, port=MYSQL_PORT, \
+                                       user=MYSQL_USER, passwd=MYSQL_PASS, \
+                                       charset="utf8")
+            connection.select_db(MYSQL_DB)
+
+            cur = connection.cursor()
+
+            sql_select = """SELECT * FROM book_table"""
+            cur.execute(sql_select)
+
+            ret_msg = ''
+            for record in cur.fetchall():
+                u=record[2].encode("utf-8")
+                ret_msg = ret_msg + u + '\n'
+        except Exception, e:
+            ret_msg = 'error: insert_error:' + e.__class__.__doc__
+
+        try:
+            myxml = '''\
+            <xml>
+            <ToUserName><![CDATA[{}]]></ToUserName>
+            <FromUserName><![CDATA[{}]]></FromUserName>
+            <CreateTime>12345678</CreateTime>
+            <MsgType><![CDATA[text]]></MsgType>
+            <Content><![CDATA[{}]]></Content></xml>
+            '''.format(recv_con['FromUserName'],recv_con['ToUserName'], ret_msg)
+            return myxml
+        except Exception, e:
+            ret_msg = ret_msg + e.__class__.__doc__
+
+            myxml = '''\
+            <xml>
+            <ToUserName><![CDATA[{}]]></ToUserName>
+            <FromUserName><![CDATA[{}]]></FromUserName>
+            <CreateTime>12345678</CreateTime>
+            <MsgType><![CDATA[text]]></MsgType>
+            <Content><![CDATA[{}]]></Content></xml>
+            '''.format(recv_con['FromUserName'],recv_con['ToUserName'], ret_msg)
+            return myxml
+        
+    elif d.startswith('qb '):
+        try:
+            connection=MySQLdb.connect(host=MYSQL_HOST_M, port=MYSQL_PORT, \
+                                       user=MYSQL_USER, passwd=MYSQL_PASS, \
+                                       charset="utf8")
+            connection.select_db(MYSQL_DB)
+
+            cur = connection.cursor()
+
+            sql_select = """SELECT * FROM book_table"""
+            cur.execute(sql_select)
+
+            ret_msg = ''
+            for record in cur.fetchall():
+                if d[3:]==record[2]:
+                    u=record[3].encode("utf-8")
+                    ret_msg = ret_msg + u + '\n'
+        except Exception, e:
+            ret_msg = 'error: insert_error:' + e.__class__.__doc__
+
+        try:
+            myxml = '''\
+            <xml>
+            <ToUserName><![CDATA[{}]]></ToUserName>
+            <FromUserName><![CDATA[{}]]></FromUserName>
+            <CreateTime>12345678</CreateTime>
+            <MsgType><![CDATA[text]]></MsgType>
+            <Content><![CDATA[{}]]></Content></xml>
+            '''.format(recv_con['FromUserName'],recv_con['ToUserName'], ret_msg)
+            return myxml
+        except Exception, e:
+            ret_msg = ret_msg + e.__class__.__doc__
+
+            myxml = '''\
+            <xml>
+            <ToUserName><![CDATA[{}]]></ToUserName>
+            <FromUserName><![CDATA[{}]]></FromUserName>
+            <CreateTime>12345678</CreateTime>
+            <MsgType><![CDATA[text]]></MsgType>
+            <Content><![CDATA[{}]]></Content></xml>
+            '''.format(recv_con['FromUserName'],recv_con['ToUserName'], ret_msg)
+            return myxml
+        
     elif d in ['?', 'h', 'help']:
         helpinfo='''使用信息:
-        u+空格+用户名 记录用户名
-        qu 查询所有用户
-        b+空格+书名 记录书名
-        ?|h|help 获取使用信息
+        0 ?|h|help 获取使用信息
+        1 u+空格+用户名 记录用户名
+        2 qu 查询所有用户
+        3 qu+空格+用户名 查询特定用户的所有图书
+        4 b+空格+书名 记录书名
+        5 qb 查询所有图书
+        6 qb+空格+图书名 查询特定图书的所有拥有者
         '''
         myxml = '''\
     <xml>
